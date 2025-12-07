aaef4a1da6fd5192337cb5f03627c091ffe19883
gpsign <gpsignorini@gmail.com>
2025-12-07 18:33:24 -0300

    init: front-end

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main>
+      <Button onClick={() => alert("cl")}>Click</Button>
+    </main>
+  );
+}
